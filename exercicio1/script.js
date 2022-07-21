e9a3d49d46337eb514e70e4ddb45926278f51eb0
Sanderson Luiz Celestino <sandersonluiz1988@gmail.com>
2022-07-21 18:46:12 -0300

    exercicio

---
+++ exercicio1/script.js
@@ -0,0 +1,13 @@
+
+let numero = Number(prompt("Escreva um número par"))
+
+console.log(numero);
+console.log(numero%2);
+
+// todo número par dividido por dois da 0 como resto.
+
+console.log(numero);
+console.log(numero%2);
+
+// todo número impar dividido por dois o resto é 1.
+
---
+++ exercicio2/script.js
@@ -0,0 +1,6 @@
+let numero = Number(prompt("Insira um número"))
+let numero2 = Number(prompt("Insira um outro número"))
+
+console.log(numero>numero2);
+console.log(numero===numero2);
+console.log();
